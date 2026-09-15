d0cad03f83956948787816c29ef4c8694d0dbbd3
Aye Myat <amn260621@gmail.com>
2026-09-15 13:37:36 +0630

    local change

---
+++ grade2.py
@@ -0,0 +1,14 @@
+name = input("Enter student name: ")
+
+mark1 = float(input("Enter first subject mark: "))
+mark2 = float(input("Enter second subject mark: "))
+
+average = (mark1 + mark2) / 2
+
+print("Student Name:", name)
+print("Average Mark:", average)
+
+if average >= 50:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
